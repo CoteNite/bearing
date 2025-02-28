c59c32357f5f46ddf9e4f78460656ef153b7e7f6
RRRRRRichardYoung <3399788235@qq.com>
2025-03-01 04:27:17 +0800

    feat: 登录功能

---
+++ src/main/java/cn/cotenite/bearing/mapper/UserMapper.java
@@ -0,0 +1,24 @@
+package cn.cotenite.bearing.mapper;
+
+
+import cn.cotenite.bearing.domain.po.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+/**
+* @author 29543
+* @description 针对表【user】的数据库操作Mapper
+* @createDate 2025-02-26 05:19:08
+* @Entity cn.cotenite.bearing.domain.User
+*/
+@Mapper
+public interface UserMapper extends BaseMapper<User> {
+
+    User selectUserById(@Param("username") String username);
+
+}
+
+
+
+
