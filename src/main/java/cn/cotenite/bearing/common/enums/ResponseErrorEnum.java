ad92f27af96270a73d7291f17bf7d380050246b3
RRRRRRichardYoung <3399788235@qq.com>
2025-03-06 09:52:45 +0800

    fix:阶段性修复bug

--- src/main/java/cn/cotenite/bearing/common/enums/ResponseErrorEnum.java
+++ src/main/java/cn/cotenite/bearing/common/enums/ResponseErrorEnum.java
@@ -11,13 +11,13 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum ResponseErrorEnum {
-    SYSTEM_ERROR("200", "出错啦，后台小哥正在努力修复中..."),
-    PARAM_NOT_VALID("200", "您传入的参数存在错误，请检查后重试"),
-    USER_NOT_FOUND("200","用户未找到"),
-    CAPTCHA_ERROR("200","验证码错误"),
-    AUTH_ERROR("200","登陆错误"),
-    PASSWORD_OR_USERNAME_FAILURE("200","用户名或密码错误"),
-    ROLE_ERROR("200","您当前的角色无权限进行该操作")
+    SYSTEM_ERROR("400", "出错啦，后台小哥正在努力修复中..."),
+    PARAM_NOT_VALID("400", "您传入的参数存在错误，请检查后重试"),
+    USER_NOT_FOUND("400","用户未找到"),
+    CAPTCHA_ERROR("400","验证码错误"),
+    AUTH_ERROR("400","登陆错误"),
+    PASSWORD_OR_USERNAME_FAILURE("400","用户名或密码错误"),
+    ROLE_ERROR("400","您当前的角色无权限进行该操作")
     ;
 
     // 异常码
